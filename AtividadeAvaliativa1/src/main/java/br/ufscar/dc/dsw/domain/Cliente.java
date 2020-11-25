34209d89130afb9a7423feb32d5da0bbca9385d1
MarcoGrivol <mbgrivol@gmail.com>
2020-11-25 17:50:07 -0300

    base crud cliente

---
+++ AtividadeAvaliativa1/src/main/java/br/ufscar/dc/dsw/domain/Cliente.java
@@ -0,0 +1,116 @@
+package br.ufscar.dc.dsw.domain;
+
+public class Cliente {
+
+	private Long id;
+	private String CPF;
+	private String data_nascimento;
+	private String email;
+	private String login;
+	private String nome;
+	private String senha;
+	private String sexo;
+	private String telefone;
+
+	public Cliente(Long id) {
+		this.id = id;
+	}
+
+	public Cliente(String CPF, String data_nascimento, String email, String login,
+				   String nome, String senha, String sexo, String telefone) {
+		super();
+		this.CPF = CPF;
+		this.data_nascimento = data_nascimento;
+		this.email = email;
+		this.login = login;
+		this.nome = nome;
+		this.senha = senha;
+		this.sexo = sexo;
+		this.telefone = telefone;
+	}
+
+	public Cliente(Long id, String CPF, String data_nascimento, String email, String login,
+			       String nome, String senha, String sexo, String telefone) {
+		super();
+		this.CPF = CPF;
+		this.data_nascimento = data_nascimento;
+		this.email = email;
+		this.login = login;
+		this.nome = nome;
+		this.senha = senha;
+		this.sexo = sexo;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCPF() {
+		return this.CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+	
+	public String getDataNascimento() {
+		return this.data_nascimento;
+	}
+
+	public void setDataNascimento(String data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSenha() {
+		return this.senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getSexo() {
+		return this.sexo;
+	}
+
+	public void setSexo(String Sexo) {
+		this.sexo = Sexo;
+	}
+	
+	public String getTelefone() {
+		return this.telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
